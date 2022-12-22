5f5b89b601adb6eee69ed6b88c97407f7277b57c
Michal-Roman0 <michal_witold.roman@wp.pl>
2022-12-22 12:40:03 +0100

    Added enemies following the player

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     Animator animator;
+    Rigidbody2D rb;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float speed = 0.5f;
+    [SerializeField]
+    public float health = 1;
     public float Health{
         set{
             if(health > value){
@@ -25,8 +32,13 @@ public class Enemy : MonoBehaviour
     }
     void Start(){
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        target = FindObjectOfType<PlayerController>().transform;
     }
-    public float health = 1;
+    public void FixedUpdate()
+    {
+        rb.MovePosition(Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.fixedDeltaTime));
+    }
     public void Defeated(){ //apart from RemoveEnemy(), so we can First play defeat animation, and then remove
         animator.SetTrigger("Defeated"); //starts defeated animation, which calls RemoveEnemy() at the end
     }
